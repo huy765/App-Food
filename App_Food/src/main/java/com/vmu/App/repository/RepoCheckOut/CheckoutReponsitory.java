738f8675a5c6783074df40dad74ec9baa811cb4d
xuanquytb <46671587+xuanquytb@users.noreply.github.com>
2021-09-18 01:05:12 +0700

    add anno pro

--- App_Food/src/main/java/com/vmu/App/repository/RepoCheckOut/CheckoutReponsitory.java
+++ App_Food/src/main/java/com/vmu/App/repository/RepoCheckOut/CheckoutReponsitory.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.vmu.App.models.CheckOut;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,4 +13,7 @@ public interface CheckoutReponsitory extends JpaRepository<CheckOut,Long> {
     List<CheckOut> findByUserid(Long userid);
     List<CheckOut> findAll();
     List<CheckOut> findByNgaytao(String ngaytao);
+
+    @Query(value = "CALL getCheckOutWithMonth('2021/09/17');", nativeQuery = true)
+    List<CheckOut> findByThang(String ngaytaopr);
 }
